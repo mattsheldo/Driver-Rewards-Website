d205316064ba173453ddc92715ae820cc2fe8fb5
Zach Amend <zach.e.amend@gmail.com>
2020-09-22 17:55:40 -0400

    Added JS that could send data to a generic backend

--- login.js
+++ login.js
@@ -1,15 +1,37 @@
+// Create a state change callback
+const myCallBack = function(){
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    // Success
+    console.log("Success");
+  }
+  else {
+    // Failure
+    console.log("Failure");
+  }
+};
+
 const loginFun = function(){
   // Retrieve username and password from HTML form
   let username = document.querySelector("#login_page [name='username']").value;
   let openPassword = document.querySelector("#login_page [name='password']").value;
 
-  // let xhr = new XMLHttpRequest();
-  // let url = "backend.py";
+  let xhr = new XMLHttpRequest();
+  let url = "backend.py";
 
-  // // Open connection to server
-  // xhr.open("POST", url, true, username, openPassword);
-  // // Set header
-  // xhr.setRequestHeader("Content-Type", "application/json");
+  // Open connection to server
+  xhr.open("POST", url, true, username, openPassword);
+  // Set header
+  xhr.setRequestHeader("Content-Type", "application/json");
+  // Set callback
+  xhr.onreadystatechange = myCallBack;
+
+  // Convert username and password from JSON to string
+  let data = JSON.stringify({
+    "username": username,
+    "password": openPassword
+  });
+  // Send data
+  xhr.send(data);
 };
 
 const registerFun = function(){
