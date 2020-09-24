f9f179e484c60e20b017b77c2e86dc69da69cc0e
Zach Amend <zach.e.amend@gmail.com>
2020-09-24 11:21:35 -0400

    Updated with Django forms

--- login.js
+++ login.js
@@ -1,39 +1,3 @@
-// Create a state change callback
-const myCallBack = function(){
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    // Success
-    console.log("Success");
-  }
-  else {
-    // Failure
-    console.log("Failure");
-  }
-};
-
-const loginFun = function(){
-  // Retrieve username and password from HTML form
-  let username = document.querySelector("#login_page [name='username']").value;
-  let openPassword = document.querySelector("#login_page [name='password']").value;
-
-  let xhr = new XMLHttpRequest();
-  let url = "connect.py";
-
-  // Open connection to server
-  xhr.open("POST", url, true);
-  // Set header
-  xhr.setRequestHeader("Content-Type", "application/json");
-  // Set callback
-  xhr.onreadystatechange = myCallBack;
-
-  // Convert username and password from JSON to string
-  let data = JSON.stringify({
-    "username": username,
-    "password": openPassword
-  });
-  // Send data
-  xhr.send(data);
-};
-
 const registerFun = function(){
   // Link to registration page
   window.location.href = "create/";
@@ -41,9 +5,6 @@ const registerFun = function(){
 
 const init = function(){
   console.log("init");
-
-  let loginBut = document.getElementById("login");
-  loginBut.onclick = loginFun;
 
   let registerBut = document.getElementById("register");
   registerBut.onclick = registerFun;
