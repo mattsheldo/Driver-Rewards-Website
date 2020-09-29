b6364b215d7b0e79cc3ecc6e4435ae9e985bee68
Zach Amend <zach.e.amend@gmail.com>
2020-09-28 20:04:21 -0400

    Changed tab to 4 (server standard)

--- AccountChecks.py
+++ AccountChecks.py
@@ -1,66 +1,66 @@
 import mysql.connector
 
-def verifyAccount(userUser, userPass):
-  # Open connection
-  try:
-    mydb = mysql.connector.connect(
-      host="cpsc4910group1rds.cwlgcbjw7kmo.us-east-1.rds.amazonaws.com",
-      user="admin",
-      password="adminpass"
-    )
-
-    # Look for a match in Drivers
-    myCursor = mydb.cursor()
-    query = "SELECT * FROM Drivers WHERE Username = '" + userUser + "';"
+def verifyAccount(userUser):
+    # Open connection
     try:
-      # Execute query and get results
-      myCursor.execute(query)
-      myResults = myCursor.fetchall()
+        mydb = mysql.connector.connect(
+        host="cpsc4910group1rds.cwlgcbjw7kmo.us-east-1.rds.amazonaws.com",
+        user="admin",
+        password="adminpass"
+        )
 
-      # If a result is found, then there's a match
-      if len(myResults) > 0:
-        # Driver found
-        five = 5
+        # Look for a match in Drivers
+        myCursor = mydb.cursor()
+        query = "SELECT * FROM Drivers WHERE Username = '" + userUser + "';"
+        try:
+            # Execute query and get results
+            myCursor.execute(query)
+            myResults = myCursor.fetchall()
+
+            # If a result is found, then there's a match
+            if len(myResults) > 0:
+                # Driver found
+                five = 5
+        except Exception:
+            print("verifyAccount(): Failed to query Drivers")
+        finally:
+            myCursor.close()
+
+        # Look for a match in Sponsors
+        myCursor = mydb.cursor()
+        query = "SELECT * FROM Sponsors WHERE Username = '" + userUser + "';"
+        try:
+            # Execute query and get results
+            myCursor.execute(query)
+            myResults = myCursor.fetchall()
+
+            # If a result is found, then there's a match
+            if len(myResults) > 0:
+                # Sponsor found
+                five = 5
+        except Exception:
+            print("verifyAccount(): Failed to query Sponsors")
+        finally:
+            myCursor.close()
+
+        # Look for a match in Admins
+        myCursor = mydb.cursor()
+        query = "SELECT * FROM Admins WHERE Username = '" + userUser + "';"
+        try:
+            # Execute query and get results
+            myCursor.execute(query)
+            myResults = myCursor.fetchall()
+
+            # If a result is found, then there's a match
+            if len(myResults) > 0:
+                # Admin found
+                five = 5
+        except Exception:
+            print("verifyAccount(): Failed to query Admins")
+        finally:
+            myCursor.close()
     except Exception:
-      print("verifyAccount(): Failed to query Drivers")
+        print("verifyAccount(): Failed to connect")
     finally:
-      myCursor.close()
-
-    # Look for a match in Sponsors
-    myCursor = mydb.cursor()
-    query = "SELECT * FROM Sponsors WHERE Username = '" + userUser + "';"
-    try:
-      # Execute query and get results
-      myCursor.execute(query)
-      myResults = myCursor.fetchall()
-
-      # If a result is found, then there's a match
-      if len(myResults) > 0:
-        # Sponsor found
-        five = 5
-    except Exception:
-      print("verifyAccount(): Failed to query Sponsors")
-    finally:
-      myCursor.close()
-
-    # Look for a match in Admins
-    myCursor = mydb.cursor()
-    query = "SELECT * FROM Admins WHERE Username = '" + userUser + "';"
-    try:
-      # Execute query and get results
-      myCursor.execute(query)
-      myResults = myCursor.fetchall()
-
-      # If a result is found, then there's a match
-      if len(myResults) > 0:
-        # Admin found
-        five = 5
-    except Exception:
-      print("verifyAccount(): Failed to query Admins")
-    finally:
-      myCursor.close()
-  except Exception:
-    print("verifyAccount(): Failed to connect")
-  finally:
-    mydb.close()
+        mydb.close()
   
