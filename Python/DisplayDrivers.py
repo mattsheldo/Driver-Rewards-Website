08fc7b904d74ec79304c3f87a5d404e4658421eb
Zach Amend <zach.e.amend@gmail.com>
2020-10-06 15:11:12 -0400

    added name instead of username to list

--- Python/DisplayDrivers.py
+++ Python/DisplayDrivers.py
@@ -1,8 +1,9 @@
 import mysql.connector
 
 class DriverListItem:
-    def __init__(self, user, pointTotal):
-        self.user = user
+    def __init__(self, first, last, pointTotal):
+        self.first = first
+        self.last = last
         self.pointTotal = pointTotal
 
 def pulldownDrivers(sponsor):
@@ -19,7 +20,7 @@ def pulldownDrivers(sponsor):
 
         # Look for all driver under the current sponsor
         myCursor = mydb.cursor()
-        query = "SELECT Drivers.Username, Drivers.Point_Total FROM Drivers JOIN Sponsors ON Drivers.Employer_ID = Sponsors.Employer_ID WHERE Sponsors.Username = '" + sponsor + "';"
+        query = "SELECT auth_user.first_name, auth_user.last_name, Drivers.Point_Total FROM (Drivers JOIN Sponsors ON Drivers.Employer_ID = Sponsors.Employer_ID) JOIN auth_user ON Drivers.Username = auth_user.username WHERE Sponsors.Username = '" + sponsor + "' ORDER BY auth_user.last_name, auth_user.first_name;"
         try:
             # Execute query and get results
             myCursor.execute(query)
@@ -27,7 +28,7 @@ def pulldownDrivers(sponsor):
 
             # Put query results into a list
             for d in myResults:
-                driverNames.append(DriverListItem(d[0], d[1]))
+                driverNames.append(DriverListItem(d[0], d[1], d[2]))
         except Exception as e:
             print("pulldownDrivers(): Failed to query database: " + str(e))
         finally:
