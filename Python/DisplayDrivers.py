3714c04c8a34432f7904c48bba6ca30470a500a4
Zach Amend <zach.e.amend@gmail.com>
2020-10-11 19:11:15 -0400

    added admin views

--- Python/DisplayDrivers.py
+++ Python/DisplayDrivers.py
@@ -21,7 +21,7 @@ def pulldownDrivers(sponsor):
 
         # Look for all drivers under the current sponsor
         myCursor = mydb.cursor()
-        query = "SELECT auth_user.first_name, auth_user.last_name, Drivers.Point_Total, auth_user.username FROM (Drivers JOIN Sponsors ON Drivers.Employer_ID = Sponsors.Employer_ID) JOIN auth_user ON Drivers.Username = auth_user.username WHERE Sponsors.Username = '" + sponsor + "' ORDER BY auth_user.last_name, auth_user.first_name;"
+        query = "SELECT auth_user.first_name, auth_user.last_name, Drivers.Point_Total, auth_user.username FROM (Drivers JOIN Sponsors ON Drivers.Employer_ID = Sponsors.Employer_ID) JOIN auth_user ON Drivers.Username = auth_user.username WHERE Sponsors.Username LIKE '" + sponsor + "' ORDER BY auth_user.last_name, auth_user.first_name;"
         try:
             # Execute query and get results
             myCursor.execute(query)
