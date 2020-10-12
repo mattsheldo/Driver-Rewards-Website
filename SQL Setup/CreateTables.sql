61b5d97cfd80c46bd64b3a3c1c2e4dc0eb12362a
Zach Amend <zach.e.amend@gmail.com>
2020-10-12 11:35:13 -0400

    RC#2

--- SQL Setup/CreateTables.sql
+++ SQL Setup/CreateTables.sql
@@ -31,7 +31,6 @@ CREATE TABLE Accounts(
 CREATE TABLE Drivers(
   Username VARCHAR(150) NOT NULL,
   Employer_ID INT NOT NULL,
-  Point_Total INT NOT NULL,
   CONSTRAINT DrPK PRIMARY KEY(Username),
   CONSTRAINT DrAccFK FOREIGN KEY(Username) REFERENCES auth_user(username),
   CONSTRAINT DrEmpFK FOREIGN KEY(Employer_ID) REFERENCES Employers(ID)
@@ -54,44 +53,59 @@ CREATE TABLE Admins(
 -- Create an entry for each point change
 CREATE TABLE Point_History(
   ID INT NOT NULL,
-  Username VARCHAR(100) NOT NULL,
+  Username VARCHAR(150) NOT NULL,
+  Employer_ID INT NOT NULL,
   Date_ DATE NOT NULL,
   Point_Cost INT NOT NULL,
   Type_Of_Change CHAR(3) NOT NULL,     -- add or sub
-  Sponsor_ID VARCHAR(25),     -- Either Sponsor_ID or Admin_ID can be NULL, but NOT BOTH
-  Admin_Id VARCHAR(25),
+  Sponsor_ID VARCHAR(150),     -- Either Sponsor_ID or Admin_ID can be NULL, but NOT BOTH
+  Admin_Id VARCHAR(150),
   CONSTRAINT PntPK PRIMARY KEY(ID),
   CONSTRAINT PntAccFK FOREIGN KEY(Username) REFERENCES Drivers(Username),
   CONSTRAINT PntSpFK FOREIGN KEY(Sponsor_ID) REFERENCES Sponsors(Username),
-  CONSTRAINT PntAdmFK FOREIGN KEY(Admin_Id) REFERENCES Admins(Username)
+  CONSTRAINT PntAdmFK FOREIGN KEY(Admin_Id) REFERENCES Admins(Username),
+  CONSTRAINT PntEmpFK FOREIGN KEY(Employer_ID) REFERENCES Employers(Employer_ID)
 );
 
 -- Create an entry for each item in a cart
 CREATE TABLE Shopping_Cart_Items(
   ID INT NOT NULL,
-  Username VARCHAR(20) NOT NULL,
+  Username VARCHAR(150) NOT NULL,
+  Employer_ID INT NOT NULL,
   Point_Cost INT NOT NULL,
-  Product_Name VARCHAR(20) NOT NULL,
-  Sponsor_ID VARCHAR(25),
-  Admin_Id VARCHAR(25),
+  Product_Name VARCHAR(150) NOT NULL,
+  Sponsor_ID VARCHAR(150),
+  Admin_Id VARCHAR(150),
   CONSTRAINT CrtPK PRIMARY KEY(ID),
   CONSTRAINT CrtAccFK FOREIGN KEY(Username) REFERENCES Drivers(Username),
   CONSTRAINT CrtSpFK FOREIGN KEY(Sponsor_ID) REFERENCES Sponsors(Username),
-  CONSTRAINT CrtAdmFK FOREIGN KEY(Admin_Id) REFERENCES Admins(Username)
+  CONSTRAINT CrtAdmFK FOREIGN KEY(Admin_Id) REFERENCES Admins(Username),
+  CONSTRAINT CrtEmpFK FOREIGN KEY(Employer_ID) REFERENCES Employers(Employer_ID)
 );
 
 -- Create an entry for each item purchased
 CREATE TABLE Purchase_History(
   ID INT NOT NULL,
-  Username VARCHAR(20) NOT NULL,
+  Username VARCHAR(150) NOT NULL,
+  Employer_ID INT NOT NULL,
   Date_ DATE NOT NULL,
   Point_Total INT NOT NULL,
-  Product_Name VARCHAR(20) NOT NULL,
+  Product_Name VARCHAR(150) NOT NULL,
   Completed BOOLEAN NOT NULL,     -- False until 24 hours has passed from time of purchase
-  Sponsor_ID VARCHAR(25),
-  Admin_Id VARCHAR(25),
+  Sponsor_ID VARCHAR(150),
+  Admin_Id VARCHAR(150),
   CONSTRAINT PchPK PRIMARY KEY(ID),
   CONSTRAINT PchAccFK FOREIGN KEY(Username) REFERENCES Drivers(Username),
   CONSTRAINT PchSpFK FOREIGN KEY(Sponsor_ID) REFERENCES Sponsors(Username),
-  CONSTRAINT PchAdmFK FOREIGN KEY(Admin_Id) REFERENCES Admins(Username)
+  CONSTRAINT PchAdmFK FOREIGN KEY(Admin_Id) REFERENCES Admins(Username),
+  CONSTRAINT PchEmpFK FOREIGN KEY(Employer_ID) REFERENCES Employers(Employer_ID)
+);
+
+CREATE TABLE Driver_Points(
+  Driver_User VARCHAR(150) NOT NULL,
+  Employer_ID INT NOT NULL,
+  Point_Total INT NOT NULL,
+  CONSTRAINT DPPk PRIMARY KEY(Driver_User, Employer_ID),
+  CONSTRAINT DPDrFK FOREIGN KEY(Driver_User) REFERENCES Drivers(Username),
+  CONSTRAINT DPEmpFK FOREIGN KEY(Employer_ID) REFERENCES Employers(Employer_ID)
 );
