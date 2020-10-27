56eed995dc4df16e2039597a9b7c8dd046891925
Zach Amend <zach.e.amend@gmail.com>
2020-10-26 23:51:28 -0400

    admin can view catalogs now

--- Python/ViewCompany.py
+++ Python/ViewCompany.py
@@ -8,6 +8,44 @@ class CompanyProfile:
         self.code = code
         self.query = query
 
+def pullAllCompanies():
+    companies = []
+
+    # Open connection
+    try:
+        mydb = mysql.connector.connect(
+            host="cpsc4910group1rds.cwlgcbjw7kmo.us-east-1.rds.amazonaws.com",
+            user="admin",
+            password="adminpass",
+            database="DriverRewards"
+        )
+
+        # Look for all info for all employers
+        myCursor = mydb.cursor()
+        query = "SELECT ID, Name_, PointsPerDollar, Join_Code, API_Keyword FROM Employers WHERE ID > -1 ORDER BY Name_, ID;"
+        try:
+            # Execute query and get results
+            myCursor.execute(query)
+            myResults = myCursor.fetchall()
+
+            # Put query results into the profile object and append to array
+            for c in myResults:
+                profileObj = CompanyProfile(c[0], c[1], c[2], c[3], c[4])
+                companies.append(profileObj)
+        except Exception as e:
+            print("pullAllCompanies(): Failed to query database: " + str(e))
+        finally:
+            myCursor.close()
+    except Exception as e:
+        print("pullAllCompanies(): Failed to connect: " + str(e))
+    finally:
+        mydb.close()
+        return companies
+
+result = pullAllCompanies()
+for r in result:
+    print(str(r.cid) + r.name + r.query)
+
 def pullCompanyProfile(sponsor):
     profileObj = CompanyProfile(0, "", 0.0, 0, "")
 
