04e7e7579724aef3405152bb7b22581106e0d539
Zach Amend <zach.e.amend@gmail.com>
2020-10-26 20:41:00 -0400

    Updated for catalog use

--- Python/DisplayDrivers.py
+++ Python/DisplayDrivers.py
@@ -100,7 +100,6 @@ def adminPulldownDrivers():
             myCursor.execute(query)
             myResults = myCursor.fetchall()
 
-            # TODO: Fix this broken logic
             # Put query results into a list
             employers = []
             # Stores the username of the last driver read in
@@ -119,7 +118,8 @@ def adminPulldownDrivers():
                     employers = []
                 
                 # Get current info
-                employers.append(DriverPoints(d[2], d[4], d[5]))
+                if d[4] > -1:
+                    employers.append(DriverPoints(d[2], d[4], d[5]))
                 item = DriverListItem(d[0], d[1], 0, d[3], employers)
         except Exception as e:
             print("pulldownDrivers(): Failed to query database: " + str(e))
